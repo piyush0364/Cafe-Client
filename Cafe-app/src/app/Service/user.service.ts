fd88210e1e10efde4032adb6016e6e402ff32aa7
Piyush Tripathi <tripathipiyush456@gmail.com>
2025-09-23 18:28:05 +0530

    Update user.service.ts

--- Cafe-app/src/app/Service/user.service.ts
+++ Cafe-app/src/app/Service/user.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class UserService {
 
   uData: User = new User();
-  readonly ApiUrl='https://cafe-mgm-a7h7a2bpbkekgxh4.eastasia-01.azurewebsites.net/api/Users';
+  readonly ApiUrl='https://cafe-mgm-fvgqcgcphwfphpbk.southeastasia-01.azurewebsites.net/api/Users';
 
   uList:User[];
 
